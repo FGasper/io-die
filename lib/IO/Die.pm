d8ade92c283a4f0804ee95dab738600e27918d37
Felipe (laptop) <felipe@cpanel.net>
2015-06-16 03:39:45 -0500

    0.042: Test improvements

--- lib/IO/Die.pm
+++ lib/IO/Die.pm
@@ -12,11 +12,11 @@ IO::Die - Namespaced, error-checked I/O
 
 =head1 VERSION
 
-Version 0.041
+Version 0.042
 
 =cut
 
-our $VERSION = '0.041';
+our $VERSION = '0.042';
 
 #----------------------------------------------------------------------
 #PROTECTED
